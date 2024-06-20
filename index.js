6fcaf453b40a853a4c436c92c345884066d826dd
yunhung-matthew <yunhung@matthewschoice.com>
2024-06-20 12:36:35 +0800

    customize first scenario

--- index.js
+++ index.js
@@ -6,8 +6,20 @@ document.addEventListener("DOMContentLoaded", function() {
     .init({ liffId: process.env.LIFF_ID })
     .then(() => {
         console.log("Success! you can do something with LIFF API here.")
+        renderHelloWorld()
     })
     .catch((error) => {
         console.log(error)
     })
 });
+
+function renderHelloWorld() {
+    const {
+        userId,
+        displayName,
+        pictureUrl,
+        statusMessage,
+    } = liff.getProfile();
+
+    document.querySelector('.welcome').innerText = `Hi~ ${displayName}!`
+}
